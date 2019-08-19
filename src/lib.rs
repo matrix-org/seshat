d4c3d7314e31f681b537f97242ada69d2c3c572e
Damir Jelić <poljar@termina.org.uk>
2019-08-19 15:21:29 +0200

    Restore the source of the event when searching.

--- src/lib.rs
+++ src/lib.rs
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::path::Path;
 use tantivy as tv;
 use tempdir::TempDir;
-use std::path::Path;
 
 pub struct Index {
     index: tv::Index,
@@ -41,9 +41,12 @@ impl Index {
 
         let index = tv::Index::open_or_create(index_dir, schema)?;
         let writer = index.writer(50_000_000)?;
-        let reader = index.reader_builder().reload_policy(tv::ReloadPolicy::OnCommit).try_into()?;
+        let reader = index
+            .reader_builder()
+            .reload_policy(tv::ReloadPolicy::OnCommit)
+            .try_into()?;
 
-        Ok(Index  {
+        Ok(Index {
             index,
             writer,
             reader,
@@ -58,25 +61,47 @@ impl Index {
         self.writer.commit().unwrap();
     }
 
-    pub fn search(&self, term: &str) -> Vec<(tv::Score, tv::DocAddress)> {
+    pub fn search(&self, term: &str) -> Vec<(f32, String)> {
+        // TODO this should be in a separate struct so we can run the search in
+        // a separate thread/task in node.
         let searcher = self.reader.searcher();
         let query_parser = tv::query::QueryParser::for_index(
             &self.index,
-            vec![self.body_field, self.topic_field, self.name_field]
+            vec![self.body_field, self.topic_field, self.name_field],
         );
 
         let query = match query_parser.parse_query(term) {
             Ok(q) => q,
-            Err(_e) => return vec![]
+            Err(_e) => return vec![],
         };
 
-        match searcher.search(&query, &tv::collector::TopDocs::with_limit(10)) {
+        let result = match searcher.search(&query, &tv::collector::TopDocs::with_limit(10)) {
             Ok(result) => result,
-            Err(_e) => vec![]
+            Err(_e) => return vec![],
+        };
+
+        let mut docs = Vec::new();
+
+        for (score, docaddress) in result {
+            let doc = match searcher.doc(docaddress) {
+                Ok(d) => d,
+                Err(_e) => continue,
+            };
+
+            let source: String = match doc.get_first(self.source_field) {
+                Some(s) => s.text().unwrap().to_owned(),
+                None => continue,
+            };
+
+            docs.push((score, source));
         }
+
+        docs
     }
 
     pub fn add_event(&mut self, body: &str, source: &str) {
+        // TODO we should only pass stuff to a queue for a thread to consume
+        // here.
         let mut doc = tv::Document::default();
         doc.add_text(self.body_field, body);
         doc.add_text(self.source_field, source);
@@ -89,9 +114,8 @@ impl Index {
 fn add_an_event() {
     let tmpdir = TempDir::new("matrix-search").unwrap();
     let mut index = Index::new(&tmpdir).unwrap();
-    index.add_event(
-        "Test message",
-        "{
+
+    let source = "{
             content: {
                 body: Test message, msgtype: m.text
             },
@@ -102,12 +126,14 @@ fn add_an_event() {
             unsigned: {age: 43289803095},
             user_id: @example2:localhost,
             age: 43289803095
-        }"
-    );
+        }";
+
+    index.add_event("Test message", source);
     index.commit();
     index.commit();
 
     let result = index.search("Test");
 
     assert_eq!(result.len(), 1);
+    assert_eq!(result[0].1, source);
 }
