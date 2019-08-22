37d3a0e1ac93a33f8a2428e2aa5af12b9eb721ab
Damir Jelić <poljar@termina.org.uk>
2019-08-22 16:38:15 +0200

    Add initial tests.

---
+++ test/test.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const expect = require('chai').expect
+
+const seshat = require('../');
+
+var matrixEvent = {
+  event_id: "$15163622445EBvZJ:localhost",
+  room_id: "!TESTROOM",
+  sender: "@alice:example.org",
+  content: {
+    body: "Test message",
+  },
+  origin_server_ts: 1516362244026,
+};
+
+var badEvent = {
+  event_id: "$15163622445EBvZJ:localhost",
+  room_id: "!TESTROOM",
+  sender: "@alice:example.org",
+  content: {
+    body: "Test message",
+  },
+  origin_server_ts: "1516362244026",
+};
+
+describe('Database', function() {
+  it('should be created succesfully.', function() {
+    var tempDir = fs.mkdtempSync(path.join(os.tmpdir(), 'seshat-'));
+    var db = new seshat.Seshat(tempDir);
+  });
+
+  var tempDir = fs.mkdtempSync(path.join(os.tmpdir(), 'seshat-'));
+  var db = new seshat.Seshat(tempDir);
+
+  it('should allow the addition of events.', function() {
+      db.add_event(matrixEvent);
+  });
+
+  it('should throw an error when adding events with missing fields.', function() {
+    delete matrixEvent.content;
+    expect(() => db.add_event(matrixEvent)).to.throw("Event doesn't contain any content");
+
+    delete matrixEvent.room_id;
+    expect(() => db.add_event(matrixEvent)).to.throw("Event doesn't contain a valid room id");
+
+    delete matrixEvent.origin_server_ts;
+    expect(() => db.add_event(matrixEvent)).to.throw("Event doesn't contain a valid timestamp");
+
+    delete matrixEvent.event_id;
+    expect(() => db.add_event(matrixEvent)).to.throw("Event doesn't contain a valid event id");
+
+    delete matrixEvent.sender;
+    expect(() => db.add_event(matrixEvent)).to.throw("Event doesn't contain a valid sender");
+  });
+
+  it('should throw an error when adding events with fields that don\'t typecheck.', function() {
+    expect(() => db.add_event(badEvent)).to.throw("Event doesn't contain a valid timestamp");
+  });
+
+});
