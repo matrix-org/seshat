3b2b9b8ff2d3e849103360c8ec64b62a9290d9dc
poljar <poljar@termina.org.uk>
2020-03-10 09:19:31 +0100

    Update src/error.rs
    Co-Authored-By: J. Ryan Stinnett <jryans@gmail.com>

--- src/error.rs
+++ src/error.rs
@@ -55,7 +55,7 @@ pub enum Error {
     #[fail(display = "Sqlcipher error: {}", _0)]
     SqlCipherError(String),
     /// Error indicating that the index needs to be rebuilt.
-    #[fail(display = "Error opening the database, the index needs to be rebuild.")]
+    #[fail(display = "Error opening the database, the index needs to be rebuilt.")]
     ReindexError,
 }
 
